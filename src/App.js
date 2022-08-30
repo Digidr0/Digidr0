1fb72c2c971b55b10601cab8e5ad10e4907adbf2
digidro <digidro.work@gmail.com>
2022-08-30 19:28:14 +0300

    Create app

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Digidro offitial page.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Digidr0"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          GitHub
         </a>
       </header>
     </div>
