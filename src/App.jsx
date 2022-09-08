0265d13650ba6254b5eb6e8392c3cfc67e40f6a3
digidro <digidro.work@gmail.com>
2022-09-08 15:46:43 +0300

    *Rebuild to Vite@latest core*

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Welcome to Digidro official page!</h1>
+     <a href='https://github.com/digidr0'>GitHub</a>
+    </div>
+  )
+}
+
+export default App
