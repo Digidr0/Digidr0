dbd78882500da82e98669d1e1ab6006a7f0cda03
Digidr0 <digidro.work@gmail.com>
2022-09-26 15:29:56 +0300

    add preload images

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -2,14 +2,14 @@ import "./About.css";
 import { useEffect, useState } from "react";
 import imgArray from "./about-images";
 import ImageScroll from "../Scroll";
-import SectionScroll from "../SectionScroll";
 
 function About() {
+  () => console.log(imgArray);
   const [index, setIndex] = useState(1);
   useEffect(() => {
-    window.addEventListener("scroll", () =>
-      setIndex(ImageScroll(30, ".About", 1.75))
-    );
+    window.addEventListener("scroll", () => {
+      setIndex(ImageScroll(30, ".About", 1.75));
+    });
   }, []);
 
   return (
@@ -17,7 +17,7 @@ function About() {
       <div className="about-description">
         <p className="about-description-text">
           Hello, my name is <b>Ildar Grankin.</b> <br></br>
-          <br></br> I am a novice js html and css developer. I also work with
+          <br></br>I am a novice js html and css developer. I also work with
           Reaсt projects and components, on which this site is written. I am
           ambitious in my approach to the project and like to learn new things
           unknown to me before. In the future I plan to study Python and
@@ -25,7 +25,7 @@ function About() {
           <br></br>In parallel I am fond of design, 3D modeling and music.
         </p>
       </div>
-      <img className="about-bg-img" src={imgArray[index]}></img>
+      <img className="about-bg-img" src={imgArray[index]} loading="lazy"></img>
     </div>
   );
 }
--- src/Components/Projects/octokit.js
+++ src/Components/Projects/octokit.js
@@ -1,7 +1,7 @@
 import { Octokit, App } from "https://cdn.skypack.dev/octokit?dts";
 export default async function GetContent(owner, repo, path) {
   const octokit = new Octokit({
-    auth: "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",
+    auth: "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",
   });
 
   const result = await octokit.request("/repos/{owner}/{repo}/contents/{path}", {
